3195fea787a83d4e30dc00a9deecdf683ad4d582
Aleksandra Pietraszek <ola.pietraszek@gmail.com>
2021-04-10 11:01:49 +0200

    profile dropdown on hover done & footer fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,34 @@
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-    console.log("house-of-plants JS imported successfully!");
+    start();
   },
   false
 );
+
+function start() {
+  console.log("house-of-plants JS imported successfully!");
+
+  const dropdown = document.querySelector(".nav-dropdown");
+  let dropdownMenuMagic;
+  console.log("HELLO");
+  dropdown?.addEventListener("mouseenter", (e) => {
+    if (dropdownMenuMagic) {
+      dropdownMenuMagic.classList.add("show-nav");
+      return;
+    }
+    const isUserLoggedIn = document.querySelector(".nav-dropdown-menu");
+    if (!isUserLoggedIn) {
+      return;
+    }
+    dropdownMenuMagic = isUserLoggedIn;
+    dropdownMenuMagic.classList.add("show-nav");
+  });
+  dropdown?.addEventListener("mouseleave", () => {
+    console.log("BYE BYE BYE");
+    if (!dropdownMenuMagic) {
+      return;
+    }
+    dropdownMenuMagic.classList.remove("show-nav");
+  });
+}
