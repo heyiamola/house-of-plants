fbc116eddffbd4f6aee8b6803f04c63eb027d502
frankie.eckersley-carr <frankie.eckersley-carr@outlook.com>
2021-04-14 21:04:44 +0200

    update form for advanced search

--- routes/search.js
+++ routes/search.js
@@ -1,13 +1,33 @@
 const express = require("express");
 
+const {
+  BERLIN_BOROUGHS,
+  PLANT_GROWING_TEMPERATURE,
+  PLANT_AVAILABILITY,
+  PLANT_GIVEAWAY_EXCHANGE,
+  PLANT_GROWING_LIGHT,
+  PLANT_GROWING_LOCATION,
+  PLANT_GROWING_WATER,
+} = require("../utils/consts");
+const Plant = require("../models/Plant.model");
+let filter = {};
+
 const router = express.Router();
 const isLoggedIn = require("../middlewares/isLoggedIn");
-const { BERLIN_BOROUGHS } = require("../utils/consts");
 
 router.get("/plant", isLoggedIn, (req, res) => {
-  res.render("search/plant", {
-    berlinBoroughs: BERLIN_BOROUGHS,
-    containsMap: true,
+  Plant.find(filter).then((foundPlants) => {
+    res.render("search/plant", {
+      foundPlants,
+      berlinBoroughs: BERLIN_BOROUGHS,
+      plantAvailability: PLANT_AVAILABILITY,
+      plantGiveawayExchange: PLANT_GIVEAWAY_EXCHANGE,
+      plantGrowingLight: PLANT_GROWING_LIGHT,
+      plantGrowingLocation: PLANT_GROWING_LOCATION,
+      plantGrowingTemperature: PLANT_GROWING_TEMPERATURE,
+      plantGrowingWater: PLANT_GROWING_WATER,
+      containsMap: true,
+    });
   });
 });
 
