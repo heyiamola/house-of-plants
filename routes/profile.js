a6b2269053fac5a416cd2b0cb1112bef99a28a4f
Aleksandra Pietraszek <ola.pietraszek@gmail.com>
2021-04-17 00:06:14 +0200

    start with edit profile video

--- routes/profile.js
+++ routes/profile.js
@@ -5,6 +5,8 @@ const isLoggedIn = require("../middlewares/isLoggedIn");
 const User = require("../models/User.model");
 const { BERLIN_BOROUGHS } = require("../utils/consts");
 
+const parser = require("../config/cloudinary");
+
 // add a route for if you go to /profile/, then you will get redirected to your own profile
 router.get("/", isLoggedIn, (req, res) => {
   res.redirect(`${req.session.user.username}`);
@@ -28,6 +30,7 @@ router.get("/:username", isLoggedIn, (req, res) => {
 });
 
 router.get("/:username/edit", isLoggedIn, (req, res) => {
+  console.log(req.session.user);
   if (req.params.username !== req.session.user.username) {
     return res.redirect("/");
   }
@@ -37,7 +40,7 @@ router.get("/:username/edit", isLoggedIn, (req, res) => {
   });
 });
 
-router.post("/edit", isLoggedIn, (req, res) => {
+router.post("/edit", isLoggedIn, parser.single("user-image"), (req, res) => {
   const {
     name,
     shortBio,
@@ -45,8 +48,8 @@ router.post("/edit", isLoggedIn, (req, res) => {
     berlinBorough,
     latitude,
     longitude,
+    profilePicture,
   } = req.body;
-
   User.findByIdAndUpdate(
     req.session.user._id, // id of the user that was logged in
     {
@@ -55,9 +58,11 @@ router.post("/edit", isLoggedIn, (req, res) => {
       email,
       berlinBorough,
       location: { coordinates: [longitude, latitude] },
+      profilePicture,
     },
     { new: true }
   ).then((updatedUser) => {
+    console.log(profilePicture);
     req.session.user = updatedUser;
     res.redirect(`/profile/${updatedUser.username}`);
   });
