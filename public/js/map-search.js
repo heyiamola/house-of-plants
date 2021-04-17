c4339d213e216b074ef69fda0e2fac9af54688a9
frankie.eckersley-carr <frankie.eckersley-carr@outlook.com>
2021-04-17 14:15:51 +0200

    working on map slider

--- public/js/map-search.js
+++ public/js/map-search.js
@@ -1,8 +1,8 @@
+let mapSliderValue = document.getElementById("map-slider").value;
+
 addMap();
 
-console.log(plantParsedLocationStr);
-
-// bufferUserLocation(userParsedLocationStr, 5);
+bufferUserLocation(userParsedLocationStr, mapSliderValue);
 
 function addMap() {
   const mapAdd = new mapboxgl.Map({
@@ -27,9 +27,57 @@ function addMap() {
   });
 }
 
+// map.on("load", function () {
+//   // Add a data source containing GeoJSON data.
+//   map.addSource("maine", {
+//     type: "geojson",
+//     data: {
+//       type: "Feature",
+//       geometry: {
+//         type: "Polygon",
+//         // These coordinates outline Maine.
+//         coordinates: [
+//           [
+//             [-67.13734, 45.13745],
+//             [-66.96466, 44.8097],
+//             [-68.03252, 44.3252],
+//             [-69.06, 43.98],
+
+//             [-67.79141, 45.70258],
+//             [-67.13734, 45.13745],
+//           ],
+//         ],
+//       },
+//     },
+//   });
+
+//   // Add a new layer to visualize the polygon.
+//   map.addLayer({
+//     id: "maine",
+//     type: "fill",
+//     source: "maine", // reference the data source
+//     layout: {},
+//     paint: {
+//       "fill-color": "#0080ff", // blue color fill
+//       "fill-opacity": 0.5,
+//     },
+//   });
+//   // Add a black outline around the polygon.
+//   map.addLayer({
+//     id: "outline",
+//     type: "line",
+//     source: "maine",
+//     layout: {},
+//     paint: {
+//       "line-color": "#000",
+//       "line-width": 3,
+//     },
+//   });
+// });
+
 function bufferUserLocation(userLocation, bufferRadius) {
   let userLocationPoint = turf.point(userLocation);
   let buffered = turf.buffer(userLocationPoint, bufferRadius);
-  // console.log(buffered);
+  console.log(buffered);
   return buffered;
 }
