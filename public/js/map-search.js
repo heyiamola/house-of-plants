9e2699323d663591334da65c9cede513cb648e33
frankie.eckersley-carr <frankie.eckersley-carr@outlook.com>
2021-04-17 15:25:13 +0200

    display buffer

--- public/js/map-search.js
+++ public/js/map-search.js
@@ -2,10 +2,10 @@ let mapSliderValue = document.getElementById("map-slider").value;
 
 addMap();
 
-bufferUserLocation(userParsedLocationStr, mapSliderValue);
+let selectCircle = bufferUserLocation(userParsedLocationStr, 5);
 
 function addMap() {
-  const mapAdd = new mapboxgl.Map({
+  const myMap = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/streets-v11",
     center: userParsedLocationStr,
@@ -23,57 +23,42 @@ function addMap() {
     new mapboxgl.Marker({})
       .setLngLat(plant.plantLocation)
       .setPopup(plantPopup)
-      .addTo(mapAdd);
+      .addTo(myMap);
+  });
+
+  myMap.addControl(new mapboxgl.NavigationControl());
+
+  myMap.on("load", function () {
+    // Add a data source containing GeoJSON data.
+    myMap.addSource("buffer", {
+      type: "geojson",
+      data: selectCircle,
+    });
+
+    // Add a new layer to visualize the polygon.
+    myMap.addLayer({
+      id: "buffer",
+      type: "fill",
+      source: "buffer", // reference the data source
+      layout: {},
+      paint: {
+        "fill-color": "#0080ff", // blue color fill
+        "fill-opacity": 0.5,
+      },
+    });
+    // Add a black outline around the polygon.
+    myMap.addLayer({
+      id: "outline",
+      type: "line",
+      source: "buffer",
+      layout: {},
+      paint: {
+        "line-color": "#000",
+        "line-width": 3,
+      },
+    });
   });
 }
-
-// map.on("load", function () {
-//   // Add a data source containing GeoJSON data.
-//   map.addSource("maine", {
-//     type: "geojson",
-//     data: {
-//       type: "Feature",
-//       geometry: {
-//         type: "Polygon",
-//         // These coordinates outline Maine.
-//         coordinates: [
-//           [
-//             [-67.13734, 45.13745],
-//             [-66.96466, 44.8097],
-//             [-68.03252, 44.3252],
-//             [-69.06, 43.98],
-
-//             [-67.79141, 45.70258],
-//             [-67.13734, 45.13745],
-//           ],
-//         ],
-//       },
-//     },
-//   });
-
-//   // Add a new layer to visualize the polygon.
-//   map.addLayer({
-//     id: "maine",
-//     type: "fill",
-//     source: "maine", // reference the data source
-//     layout: {},
-//     paint: {
-//       "fill-color": "#0080ff", // blue color fill
-//       "fill-opacity": 0.5,
-//     },
-//   });
-//   // Add a black outline around the polygon.
-//   map.addLayer({
-//     id: "outline",
-//     type: "line",
-//     source: "maine",
-//     layout: {},
-//     paint: {
-//       "line-color": "#000",
-//       "line-width": 3,
-//     },
-//   });
-// });
 
 function bufferUserLocation(userLocation, bufferRadius) {
   let userLocationPoint = turf.point(userLocation);
